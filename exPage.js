5420346a03b203c9ca03319d6d45106fe8c2158c
Mats Ljunggren <mljunggren@gmail.com>
2018-03-15 11:02:40 -0400

    Cleanup exPage

--- exPage.js
+++
@@ -1,9 +0,0 @@
-{
-  _tag:"div",
-  _attr:{
-    style:function(){
-      return a.data.page?"background:red":"background:yellow"
-    }
-  },
-  _text:"a.data.page?'load static page':'No static page'"
-}
