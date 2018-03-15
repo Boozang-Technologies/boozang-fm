fcce93ed8111d827a0880a638d79badbf32afc24
Mats Ljunggren <mljunggren@gmail.com>
2018-03-15 11:01:13 -0400

    Fixed template code

---
+++ templates/temp1.js
@@ -0,0 +1,9 @@
+{
+  _tag:"div",
+  _attr:{
+    style:function(){
+      return a.data.page?"background:green":"background:blue"
+    }
+  },
+  _text:"a.data.page?'Static page has been loaded ':'No static page loaded'"
+}
